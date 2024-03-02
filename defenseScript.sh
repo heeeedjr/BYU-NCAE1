299e70862ac8a4215676e65808a781e4cd767d17
Kyle Fallon <59681487+fallonkyle@users.noreply.github.com>
2024-03-01 18:35:08 -0700

    uploaded monitoring script

---
+++ defenseScript.sh
@@ -0,0 +1,31 @@
+touch /home/blueteam/report.txt
+##
+echo "#" > /home/blueteam/report.txt
+echo "# Contents of /etc/passwd" >> /home/blueteam/report.txt
+echo "#" >> /home/blueteam/report.txt
+cat /etc/passwd >> /home/blueteam/report.txt
+echo "" >> /home/blueteam/report.txt
+##
+echo "#" >> /home/blueteam/report.txt
+echo "# Result of netstat -tuln" >> /home/blueteam/report.txt
+echo "#" >> /home/blueteam/report.txt
+netstat -tuln >> /home/blueteam/report.txt
+echo "" >> /home/blueteam/report.txt
+##
+echo "#" >> /home/blueteam/report.txt
+echo "# Result of ps aux" >> /home/blueteam/report.txt
+echo "#" >> /home/blueteam/report.txt
+ps aux >> /home/blueteam/report.txt
+echo "" >> /home/blueteam/report.txt
+##
+echo "#" >> /home/blueteam/report.txt
+echo "# Result of /etc/apt/sources.list" >> /home/blueteam/report.txt
+echo "#" >> /home/blueteam/report.txt
+cat /etc/apt/sources.list >> /home/blueteam/report.txt
+echo "" >> /home/blueteam/report.txt
+##
+echo "#" >> /home/blueteam/report.txt
+echo "# Result of /etc/sudoers" >> /home/blueteam/report.txt
+echo "#" >> /home/blueteam/report.txt
+cat /etc/sudoers >> /home/blueteam/report.txt
+echo "" >> /home/blueteam/report.txt
